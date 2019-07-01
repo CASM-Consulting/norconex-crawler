13b0548404edc7746c35848cf3ffa16a8f954981
jack <jp242@sussex.ac.uk>
2019-07-01 11:34:06 +0100

    organising imports

--- src/main/java/uk/ac/susx/tag/norconex/document/ArticleExtractorChecksum.java
+++ src/main/java/uk/ac/susx/tag/norconex/document/ArticleExtractorChecksum.java
@@ -1,17 +1,22 @@
 package uk.ac.susx.tag.norconex.document;
 
+// boilerpipe imports
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+
+// javax imports
+import javax.xml.stream.XMLStreamException;
+
+// java imports
+import java.io.IOException;
+import java.io.StringWriter;
+
 import com.norconex.collector.core.checksum.AbstractDocumentChecksummer;
 import com.norconex.collector.core.checksum.ChecksumUtil;
 import com.norconex.commons.lang.xml.EnhancedXMLStreamWriter;
 import com.norconex.importer.doc.ImporterDocument;
-import de.l3s.boilerpipe.BoilerpipeProcessingException;
-import de.l3s.boilerpipe.extractors.CommonExtractors;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.io.IOUtils;
-
-import javax.xml.stream.XMLStreamException;
-import java.io.IOException;
-import java.io.StringWriter;
 
 public class ArticleExtractorChecksum extends AbstractDocumentChecksummer {
 
@@ -27,7 +32,7 @@ public class ArticleExtractorChecksum extends AbstractDocumentChecksummer {
         try {
             IOUtils.copy(document.getContent(), sw, document.getContentEncoding());
         } catch (IOException e) {
-            throw new RuntimeException("ERROR: Failed to retrieve web content for url: " + document.getReference());
+            throw new RuntimeException("ERROR: Failed to retrieve content for url: " + document.getReference());
         }
 
         final String html = sw.toString();
